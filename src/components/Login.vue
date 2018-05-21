655bd25f5e94b8b023d12301bcedd6e6e5c38ab4
Rafael Thayto <rafa.thayto@gmail.com>
2018-05-21 16:45:01 -0300

    Fix form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,54 +1,86 @@
 <template>
   <v-content>
+
     <form>
+      <!-- Email -->
       <v-text-field
         v-model="email"
-        :error-messages="emailErrors"
         label="E-mail"
+        :error-messages="emailErrors"
         required
         @input="$v.email.$touch()"
         @blur="$v.email.$touch()"
       ></v-text-field>
+      <!-- Password -->
       <v-text-field
         v-model="password"
-        :error-messages="passwordErrors"
         label="Senha"
+        :append-icon="passwordNotVisible ? 'visibility' : 'visibility_off'"
+        :append-icon-cb="() => (passwordNotVisible = !passwordNotVisible)"
+        :error-messages="passwordErrors"
+        :counter="20"
+        :type="passwordNotVisible ? 'password' : 'text'"
         required
-        @change="$v.password.$touch()"
+        min="6"
+        @input="$v.password.$touch()"
         @blur="$v.password.$touch()"
       ></v-text-field>
 
       <v-btn @click="submit">submit</v-btn>
-      <v-btn @click="clear">clear</v-btn>
     </form>
+
   </v-content>
 </template>
 
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, email } from 'vuelidate/lib/validators'
-import axios from 'axios'
 
 export default {
   mixins: [validationMixin],
   name: 'login',
-  data: () => ({
-    password: '',
-    email: ''
-  }),
+  validations: {
+    email: { required, email },
+    password: { required, maxLength: maxLength(20) }
+  },
+  data () {
+    return {
+      email: '',
+      password: '',
+      passwordNotVisible: true
+    }
+  },
   computed: {
-
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Coloque um e-mail válido')
+      !this.$v.email.required && errors.push('E-mail é obrigatório')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.maxLength && errors.push('A senha deve ter entre 6 e 20 caracteres')
+      !this.$v.password.required && errors.push('Senha é obrigatória')
+      return errors
+    }
   },
   methods: {
     submit () {
       this.$v.$touch()
-      axios.post('http://localhost:8080/v1/usuarios/auth', {
-        email: this.email,
-        senha: this.password
-      })
-    },
-    clear () {
-      this.$v.$reset()
+      // fetch('http://localhost:8080/ianesws/v1/usuarios/auth').
+      fetch('http://localhost:8080/ianesws/v1/usuarios/auth', {
+        method: 'POST',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify({
+          email: this.email,
+          senha: this.password
+        })
+      }).then((res) => console.log(res))
+        .catch((e) => console.log(`Error: ${e}`))
     }
   }
 }
